3358e4a227f03bbcd94a6838963feaec7e0fc823
sheikhsalman08 <mdsalman01755@gmail.com>
2017-12-25 18:29:48 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,79 @@
+<?php
+	
+	# Start the session 
+	session_start();
+	
+	# Autoload the required files
+	require_once __DIR__ . '/vendor/autoload.php';
+
+	# Set the default parameters
+	$fb = new Facebook\Facebook(
+		[
+		'app_id' => '1262936877139877',
+		'app_secret' => '383bb9e87d36ba5a5e0777cba60a3779',
+		'default_graph_version' => 'v2.11',
+		]
+	);
+	$redirect = 'http://vaidroad.com/test/';
+
+
+	# Create the login helper object
+	$helper = $fb->getRedirectLoginHelper();
+	
+	# Get the access token and catch the exceptions if any
+	try {
+	  $accessToken = $helper->getAccessToken();
+	} catch(Facebook\Exceptions\FacebookResponseException $e) {
+	  // When Graph returns an error
+	  echo 'Graph returned an error: ' . $e->getMessage();
+	  exit;
+	} catch(Facebook\Exceptions\FacebookSDKException $e) {
+	  // When validation fails or other local issues
+	  echo 'Facebook SDK returned an error: ' . $e->getMessage();
+	  exit;
+	}
+
+	# If the 
+	if (isset($accessToken)) {
+	  	// Logged in!
+	 	// Now you can redirect to another page and use the
+  		// access token from $_SESSION['facebook_access_token'] 
+  		// But we shall we the same page
+
+		// Sets the default fallback access token so 
+		// we don't have to pass it to each request
+		$fb->setDefaultAccessToken($accessToken);
+
+		try {
+		  $response = $fb->get('/me?fields=email,name');
+		  $userNode = $response->getGraphUser();
+		}catch(Facebook\Exceptions\FacebookResponseException $e) {
+		  // When Graph returns an error
+		  echo 'Graph returned an error: ' . $e->getMessage();
+		  exit;
+		} catch(Facebook\Exceptions\FacebookSDKException $e) {
+		  // When validation fails or other local issues
+		  echo 'Facebook SDK returned an error: ' . $e->getMessage();
+		  exit;
+		}
+
+
+		// Print the user Details
+		echo "Welcome !<br><br>";
+		echo 'Name: ' . $userNode->getName().'<br>';
+		echo 'User ID: ' . $userNode->getId().'<br>';
+		echo 'Email: ' . $userNode->getProperty('email').'<br><br>';
+
+		$image = 'https://graph.facebook.com/'.$userNode->getId().'/picture?width=200';
+		echo "Picture<br>";
+		echo "<img src='$image' /><br><br>";
+		
+	}else{
+		$permissions  = ['email'];
+		$loginUrl = $helper->getLoginUrl($redirect,$permissions);
+		echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
+	}
+
+
+$age = 19;
+
